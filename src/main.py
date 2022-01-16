ddcc3ab404a3f8e554c409109a070dfe75bce59c
vmesc <v.meschersky@gmail.com>
2022-01-16 18:05:39 +0300

    Added checking match count for a player

--- src/main.py
+++ src/main.py
@@ -1,25 +1,30 @@
 import requests
+import os
 from src.model import faceit_player
+
 
 app_auth_key = "bf8c2a3e-c583-4411-8e6d-dc14aaaa529b"
 players_array = []
 players_ids = open("players_ids.txt")
 
 
-def get_user_data(user_id):
-    faceit_response = requests.get(f"https://open.faceit.com/data/v4/players/{user_id}",
+def get_user_data(user_id, params):
+    request_string = f"https://open.faceit.com/data/v4/players/{user_id}" + params
+    faceit_response = requests.get(request_string,
                                    headers={"Authorization": "Bearer " + app_auth_key,
                                             "Content-Type": "application/json"})
+
     return faceit_response.json()
 
 
 def check_elo_difference(player_to_check):
     player_elo_difference = ""
+
     # getting last saved elo from array (DB)
     player_current_elo = player_to_check.player_elo  # We should get this information from DB on every iteration, right?
 
     # getting updated user information from faceit
-    player_updated_info = get_user_data(player_to_check.player_id)
+    player_updated_info = get_user_data(player_to_check.player_id, "")
 
     # getting update user elo
     player_updated_elo = player_updated_info["games"]["csgo"]["faceit_elo"]
@@ -33,15 +38,24 @@ def check_elo_difference(player_to_check):
 
 
 for player in players_ids:
-    user_info = get_user_data(player)
+    # Getting user information from faceit endpoint (no additional parameters needed)
+    user_info = get_user_data(player, "")
+
+    # Getting player match statistics from faceit "/stats/csgo/" endpoint
+    user_matches = get_user_data(player, "/stats/csgo")
+    player_matches_count = user_matches["lifetime"]
+
+    # Creating player with parameters: name, ID, ELO, matches
     current_player = faceit_player.Player(user_info["nickname"], user_info["player_id"],
-                                          user_info["games"]["csgo"]["faceit_elo"])
+                                          user_info["games"]["csgo"]["faceit_elo"], player_matches_count["Matches"])
+
     players_array.append(current_player)
 
 for player in players_array:
     elo_difference = check_elo_difference(player)
 
     if elo_difference == 0:
+        # I assume, no point to post anything in this case
         print(player.player_name + " stays on his level with " + str(player.player_elo) + " elo.")
     elif elo_difference < 0:
         print(player.player_name + " lost " + str(abs(elo_difference)) + " elo in the last game.")
--- src/model/faceit_player.py
+++ src/model/faceit_player.py
@@ -1,8 +1,10 @@
 class Player:
-    def __init__(self, name, id, elo):
+    def __init__(self, name, id, elo, matches_count):
         self.player_name = name
         self.player_id = id
         self.player_elo = elo
+        self.player_matches_count = matches_count
 
     def __str__(self):
-        return 'Id: '+ self.player_id + ', Name: '+self.player_name+', Elo:' + str(self.player_elo)
+        return 'Id: ' + self.player_id + ', Name: ' + self.player_name + ', Elo:' + str(
+            self.player_elo) + ', Matches: ' + self.player_matches_count
